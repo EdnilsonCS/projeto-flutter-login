7683960edb4a4b1bb975a0a0acbeeefdd9a9cb31
Ednilson Cardoso Dos Santos <201700017232@COSIT-242151.ufs.internal>
2019-07-09 10:13:58 -0300

    git inicial

---
+++ exemplo_menu_login/lib/Utils/Cadastro.dart
@@ -0,0 +1,8 @@
+class Cadastrar {
+  const Cadastrar({this.nome, this.email, this.cargo, this.detalhe});
+  final String nome;
+  final String email;
+  final String cargo;
+  final String detalhe;
+}
+
